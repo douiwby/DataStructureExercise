eb21c7ccf9a387c5f77f16aae42b4c8d0809972f
douiwby <douiwby@hotmail.com>
2020-04-15 22:16:11 +0800

    feat: Add find_if algorithm. Add [erase range, move/swap element, insertionSort, unique] algorithms of List.

--- DataStructure/Header/Algorithm.h
+++ DataStructure/Header/Algorithm.h
@@ -11,4 +11,34 @@ namespace Algorithm
 		}
 		return e;
 	}
+
+	template<typename Iterator, typename UnaryPredicate>
+	Iterator find_if(Iterator b, Iterator e, UnaryPredicate pred)
+	{
+		for (; b != e; ++b)
+		{
+			if (pred(*b)) return b;
+		}
+		return e;
+	}
+}
+
+#include <iostream>
+
+template <typename Iterator>
+void printByIterator(Iterator b, Iterator e)
+{
+	for (; b != e; )
+	{
+		std::cout << *b;
+		++b;
+		if (b != e) std::cout << ",";
+	}
+	std::cout << std::endl;
+}
+
+template<typename Container>
+void printContainer(const Container& c)
+{
+	printByIterator(c.begin(), c.end());
 }
--- DataStructure/Header/List.h
+++ DataStructure/Header/List.h
@@ -40,16 +40,26 @@ public:
 	ListIterator insert(const ListIterator& it, const T& v);
 	ListIterator insert(const ListIterator& it, const ListIterator& b, const ListIterator& e);
 	ListIterator erase(const ListIterator& it);
+	ListIterator erase(const ListIterator& b, const ListIterator& e);
 
 	ListIterator find(const T& v) { return Algorithm::find(begin(), end(), v); }
+
+	ListIterator searchFirstGreater(const T&v, ListIterator b, ListIterator e)
+	{
+		return Algorithm::find_if(b, e, [&v](const T& element) {return v < element; });
+	}
+	static ListIterator moveElement(const ListIterator& src, const ListIterator& dest);
+	static void swapElement(ListIterator lhs, ListIterator rhs);
+	void insertionSort(ListIterator b, const ListIterator& e);
+	void insertionSort() { insertionSort(begin(), end()); }
+	void unique();
+
 private:
 	ListNode<T>* header;
 	ListNode<T>* trailer;
 	SizeType _size;
 
 	void init();
-public:
-	void printList() const;
 };
 
 template<typename T>
@@ -65,32 +75,32 @@ public:
 	}
 	ListIterator& operator++()
 	{
-		assert(pNode && pNode->next);
+		assert(pNode && pNode->next);  //Can not increase at trailer
 		pNode = pNode->next;
 		return *this;
 	}
 	ListIterator operator++(int)
 	{
-		assert(pNode && pNode->next);
+		assert(pNode && pNode->next);  //Can not increase at trailer
 		ListIterator ret(pNode);
 		pNode = pNode->next;
 		return ret;
 	}
 	ListIterator& operator--()
 	{
-		assert(pNode && pNode->prev && pNode->prev->prev);  //Can not decrease at begin()
+		assert(pNode && pNode->prev && pNode->prev->prev);  //Can not decrease at header and begin()
 		pNode = pNode->prev;
 		return *this;
 	}
 	ListIterator operator--(int)
 	{
-		assert(pNode && pNode->prev && pNode->prev->prev);  //Can not decrease at begin()
+		assert(pNode && pNode->prev && pNode->prev->prev);  //Can not decrease at header and begin()
 		ListIterator ret(pNode);
 		pNode = pNode->prev;
 		return ret;
 	}
-	bool operator==(const ListIterator& rhs) { return this->pNode == rhs.pNode; }
-	bool operator!=(const ListIterator& rhs) { return !(*this == rhs); }
+	bool operator==(const ListIterator& rhs) const { return this->pNode == rhs.pNode; }
+	bool operator!=(const ListIterator& rhs) const { return !(*this == rhs); }
 private:
 	ListNode<T>* pNode;
 };
@@ -127,9 +137,9 @@ void List<T>::clear()
 }
 
 template<typename T>
-typename List<T>::ListIterator List<T>::insert(const ListIterator& it, const T& v)
+inline typename List<T>::ListIterator List<T>::insert(const ListIterator& it, const T& v)
 {
-	assert(it.pNode && it.pNode->prev);  //Can not insert before header
+	//assert(it.pNode && it.pNode->prev);  //Can not insert before header //Assume the Iterator is in range of begin() and end()
 	ListNode<T>* pNewNode = new ListNode<T>(v,it.pNode->prev,it.pNode);
 	it.pNode->prev->next = pNewNode;
 	it.pNode->prev = pNewNode;
@@ -140,19 +150,20 @@ typename List<T>::ListIterator List<T>::insert(const ListIterator& it, const T& 
 template<typename T>
 typename List<T>::ListIterator List<T>::insert(const ListIterator& it, const ListIterator& b, const ListIterator& e)
 {
-	assert(it.pNode && it.pNode->prev);  //Can not insert before header
+	//assert(it.pNode && it.pNode->prev);  //Can not insert before header //Assume the Iterator is in range of begin() and end()
 	ListNode<T>* prev = it.pNode->prev;
 	for (ListIterator _begin = b; _begin != e; ++_begin)
 	{
+		assert(_begin.pNode && _begin.pNode->next); //Can not read end()
 		insert(it, *_begin);
 	}
 	return prev->next;
 }
 
 template<typename T>
-typename List<T>::ListIterator List<T>::erase(const ListIterator& it)
+inline typename List<T>::ListIterator List<T>::erase(const ListIterator& it)
 {
-	assert(it.pNode && it.pNode->next);  //Can not erase at trailer
+	assert(it.pNode && it.pNode->next);  //Can not erase end()
 	ListNode<T>* ret = it.pNode->next;
 	it.pNode->prev->next = it.pNode->next;
 	it.pNode->next->prev = it.pNode->prev;
@@ -161,15 +172,75 @@ typename List<T>::ListIterator List<T>::erase(const ListIterator& it)
 	return ret;
 }
 
-#include <iostream>
+template<typename T>
+typename List<T>::ListIterator List<T>::erase(const ListIterator& b, const ListIterator& e)
+{
+	assert(b.pNode && b.pNode->next);  //Can not erase end()
+	if (b == e) return e;
+	ListIterator last(e.pNode->prev);
+	ListIterator beforeFirst(b.pNode->prev);
+	b.pNode->prev->next = e.pNode;
+	e.pNode->prev = b.pNode->prev;
+	for (; last != beforeFirst;)
+	{
+		ListIterator toBeDeleted = last--;
+		delete toBeDeleted.pNode;
+		--_size;
+	}
+	return e;
+}
 
 template<typename T>
-void List<T>::printList() const
+inline typename List<T>::ListIterator List<T>::moveElement(const ListIterator & src, const ListIterator & dest)
 {
-	for (auto it = begin(); it != end(); ++it)
+	assert(src.pNode && src.pNode->next);  //Can not move end()
+	src.pNode->prev->next = src.pNode->next;
+	src.pNode->next->prev = src.pNode->prev;
+	src.pNode->prev = dest.pNode->prev;
+	src.pNode->next = dest.pNode;
+	dest.pNode->prev->next = src.pNode;
+	dest.pNode->prev = src.pNode;
+	return src;
+}
+
+template<typename T>
+inline void List<T>::swapElement(ListIterator lhs, ListIterator rhs)
+{
+	assert(lhs.pNode && lhs.pNode->next);  //Can not move end()
+	assert(rhs.pNode && rhs.pNode->next);  //Can not move end()
+	ListIterator lElement = lhs++;
+	ListIterator rElement = rhs++;
+	moveElement(lElement, rhs);
+	moveElement(rElement, lhs);
+}
+
+template<typename T>
+void List<T>::insertionSort(ListIterator b, const ListIterator& e)
+{
+	assert(b.pNode && b.pNode->next);  //Can not move end()
+	if (b == e) return;
+	ListIterator it(b.pNode->next);
+	for (; it != e;)
 	{
-		std::cout << *it;
-		if (it != --end()) std::cout << ", ";
+		ListIterator nextIterator(it.pNode->next);
+		ListIterator searchResult = searchFirstGreater(*it, b, it);
+		if (searchResult != it) moveElement(it, searchResult);
+		if (searchResult == b) b = --searchResult; // always keep b as the first iterator
+		it = nextIterator;
 	}
-	std::cout << std::endl;
+}
+
+template<typename T>
+void List<T>::unique()
+{
+	if (_size < 2) return;
+	for (auto it = begin(); it != end();)
+	{
+		auto first = it++;
+		if (*first == *it)
+		{
+			erase(it);
+			it = first;
+		}
+	}
 }
